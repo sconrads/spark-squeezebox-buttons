d7c836ea3c7bc3356730e9d44517246d442c4731
Stian Conradsen <Stian@Stians-MacBook-4.local>
2014-12-09 22:04:31 +0100

    La til flere kanaler

--- squeezebox-button-control.cpp
+++ squeezebox-button-control.cpp
@@ -1,5 +1,5 @@
 // This #include statement was automatically added by the Spark IDE.
-#include "HttpClient.h"
+#include "HttpClient/HttpClient.h"
 
 int button = 0;
 int buttonOff = 0;
@@ -8,6 +8,8 @@ int ekstraLed = D0;
 
 int buttonPressed = 0;
 int buttonOffPressed = 0;
+int channel = 0;
+int power = 0;
 
 String responseBody;
 int responseStatus = 0;
@@ -34,20 +36,20 @@ void setup()
     pinMode(D2, INPUT);
     pinMode(led, OUTPUT);
     pinMode(ekstraLed, OUTPUT);
-    digitalWrite(led, LOW);
+    digitalWrite(led, LOW);   
     digitalWrite(ekstraLed, LOW);
 }
 void loop()
 {
   button = digitalRead(D1);
   buttonOff = digitalRead(D2);
-
+  
   if(button == 1)
   {
     if (buttonPressed == 0) {
-        digitalWrite(led, HIGH);
+        digitalWrite(led, HIGH);   
         Serial.println(WiFi.localIP());
-
+        
         Serial.print("Core IP: ");
         Serial.println(WiFi.localIP());
         Serial.print("Gateway: ");
@@ -59,13 +61,24 @@ void loop()
         IPAddress remoteIP(10,0,1,42);
         request.ip = remoteIP;
         request.port = 9000;
-
-        //NRK p3
-        //request.path = "/status.txt?p0=play&player=00:04:20:17:92:0a&uid=1417950431000";
-
-        //NRK julekanalen
-        request.path = "/status.txt?p0=play&player=00:04:20:17:92:0a&uid=1418065715000";
-
+        
+        if (channel == 0) {
+            //NRK P1
+            request.path = "/plugins/Favorites/index.html?action=play&index=3&player=00:04:20:17:92:0a";
+            channel = 1;
+        } else if (channel == 1) {
+            //NRK p3
+            request.path = "/plugins/Favorites/index.html?action=play&index=4&player=00:04:20:17:92:0a";
+            channel = 2;
+        } else {
+            //Julesanger
+            digitalWrite(ekstraLed, HIGH);
+            request.path = "/plugins/Favorites/index.html?action=play&index=7&player=00:04:20:17:92:0a";
+            delay(1000);
+            digitalWrite(ekstraLed, LOW);  
+            channel = 0;
+        }
+     
         // Get request
         http.get(request, response, headers);
         Serial.print("Application>\tResponse status: ");
@@ -77,20 +90,20 @@ void loop()
         if (response.status == 200)
         {
             delay(1000);
-            digitalWrite(led, LOW);
+            digitalWrite(led, LOW);   
             delay(1000);
-            digitalWrite(led, HIGH);
+            digitalWrite(led, HIGH);   
             delay(1000);
-            digitalWrite(led, LOW);
+            digitalWrite(led, LOW);      
         } else
         {
             digitalWrite(ekstraLed, HIGH);
             delay(1000);
-            digitalWrite(ekstraLed, LOW);
+            digitalWrite(ekstraLed, LOW);  
             delay(1000);
             digitalWrite(ekstraLed, HIGH);
             delay(1000);
-            digitalWrite(ekstraLed, LOW);
+            digitalWrite(ekstraLed, LOW);  
         }
     }
     buttonPressed = 1;
@@ -98,13 +111,13 @@ void loop()
   {
     buttonPressed = 0;
   }
-
+  
   if(buttonOff == 1)
   {
     if (buttonOffPressed == 0) {
-        digitalWrite(led, HIGH);
+        digitalWrite(led, HIGH);   
         Serial.println(WiFi.localIP());
-
+        
         Serial.print("Core IP: ");
         Serial.println(WiFi.localIP());
         Serial.print("Gateway: ");
@@ -116,9 +129,15 @@ void loop()
         IPAddress remoteIP(10,0,1,42);
         request.ip = remoteIP;
         request.port = 9000;
-
-        request.path = "/status.txt?p0=button&p1=power_off&player=00:04:20:17:92:0a";
-
+        
+        if (power == 0) {
+            request.path = "/status.txt?p0=button&p1=power_off&player=00:04:20:17:92:0a"; 
+            power = 1;
+        } else {
+            request.path = "/status.txt?p0=play&player=00:04:20:17:92:0a"; 
+            power = 0;
+        }
+       
         // Get request
         http.get(request, response, headers);
         Serial.print("Application>\tResponse status: ");
@@ -130,20 +149,20 @@ void loop()
         if (response.status == 200)
         {
             delay(1000);
-            digitalWrite(led, LOW);
+            digitalWrite(led, LOW);   
             delay(1000);
-            digitalWrite(led, HIGH);
+            digitalWrite(led, HIGH);   
             delay(1000);
-            digitalWrite(led, LOW);
+            digitalWrite(led, LOW);      
         } else
         {
             digitalWrite(ekstraLed, HIGH);
             delay(1000);
-            digitalWrite(ekstraLed, LOW);
+            digitalWrite(ekstraLed, LOW);  
             delay(1000);
             digitalWrite(ekstraLed, HIGH);
             delay(1000);
-            digitalWrite(ekstraLed, LOW);
+            digitalWrite(ekstraLed, LOW);  
         }
     }
     buttonOffPressed = 1;
@@ -151,5 +170,5 @@ void loop()
   {
     buttonOffPressed = 0;
   }
-
+  
 }
